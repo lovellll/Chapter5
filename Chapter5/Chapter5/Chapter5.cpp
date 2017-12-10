3038fcd5dc7710efd759db2ff4f1bb812c5a6ef8
Geek song <503455810@qq.com>
2017-12-10 15:09:47 +0800

    caculator

--- Chapter5/Chapter5/Chapter5.cpp
+++ Chapter5/Chapter5/Chapter5.cpp
@@ -2,10 +2,47 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+int caculate(int x, int y, char operations)
+{
+	switch (operations)
+	{
+		case '+':
+			return x + y;
+		case '-':
+			return x - y;
+		case '*':
+			return x * y;
+		case '/':
+			return x / y;
+		case '%':
+			return x % y;
+
+	}
+}
+
+
 
 
 int main()
 {
+	int x;
+	std::cout << "please input the first one number\n";
+	std::cin >> x;
+
+	char oprations;
+	std::cout << "please input the oprations\n";
+	std::cin >> oprations;
+
+	int y;
+	std::cout << "please input the second one number\n";
+	std::cin >> y;
+
+	std::cout << "your answer is " << caculate(x, y, oprations) << '\n';
+
+
+
     return 0;
 }
 
